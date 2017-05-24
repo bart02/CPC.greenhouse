65d5bbdb5213e98624acc77347fb7f9899a4d7df
Artem Batalov <bat.artem2002@ya.ru>
2017-05-24 15:47:15 +0700

    Automatic port search

--- python/start.py
+++ python/start.py
@@ -12,27 +12,54 @@ import serial
 import time
 from threading import Thread
 
+
 variables = {'data1': [], 'data2': 0}
 
+
+def search():
+    found = False
+    for j in range(2):
+        for i in range(64):
+            try:
+                if j == 0:
+                    port = "/dev/ttyUSB" + str(i)
+                if j == 1:
+                    port = "COM" + str(i)
+                ser = serial.Serial(port)
+                ser.close()
+                found = True
+                break
+            except serial.serialutil.SerialException:
+                pass
+
+        if found:
+            return port
+            break
+    if not found:
+        raise NameError("Ports not found")
+
+
 def recieving_data():
-	while True:
-		data = ser.readline().decode('utf-8')
-		if data[0] == '.':
-			if data[1] == ',':
-				data = data[2:].split('!')[0].split('@')
-				variables['data1'] = data
+    while True:
+        data = ser.readline().decode('utf-8')
+        if data[0] == '.':
+            if data[1] == ',':
+                data = data[2:].split('!')[0].split('@')
+                variables['data1'] = data
+
 
 def make_global():
-	global variables
-	
+    global variables
+
+
 make_global()
 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial(search(), 9600)
 time.sleep(0.3)
 
 recieving = Thread(target=recieving_data)
 recieving.start()
 
 while True:
-	print(variables['data1'])
-	time.sleep(2)
+    print(variables['data1'])
+    time.sleep(2)
