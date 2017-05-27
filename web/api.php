b39dbcad8f2c8ab1edd467928714f9b3454905c0
Artem Batalov <bat.artem2002@ya.ru>
2017-05-27 22:00:58 +0700

    Add api for send data from python

---
+++ web/api.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(1);
+
+$mysql_s = "92.127.158.65";    #MySQL server
+$mysql_u = "dc319_greeghouse";    #MySQL user
+$mysql_p = 'n4z8YKCHZ82W';    #MySQL password
+$mysql_d = "dc319_greeghouse";    #MySQL database
+try {
+	$db = new PDO('mysql:host='.$mysql_s.';dbname='.$mysql_d, $mysql_u, $mysql_p, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+} catch (PDOException $e) {
+	exit($e->getMessage());
+}
+
+
+
+if ($_REQUEST['key'] == '30bJpP0R29epB7kofxF5WszPtP1fRJxWbVEf89bDXOFJpEJRMdvTN6ouqXOtg2bb' && isset($_REQUEST['type']) && isset($_REQUEST['data'])) {
+	echo "LOGINED!!!";
+	$db->query("INSERT INTO `logs`(`logtype`, `date`, `data`) VALUES (" . $db->quote($_REQUEST['type']) . ", " . $db->quote(date('d.m H:i')) . ",". $db->quote($_REQUEST['data']) . ")");
+} else {
+	echo "TbI DYPAK";
+}
+
+?>
