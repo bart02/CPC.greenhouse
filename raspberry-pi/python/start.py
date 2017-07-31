371b148d4fb8fba04c9d5992edad4158436256dc
Artem Batalov <bat.artem2002@ya.ru>
2017-07-31 14:03:16 +0700

    new test

--- raspberry-pi/python/start.py
+++ raspberry-pi/python/start.py
@@ -48,7 +48,7 @@ def receiving_data():   # Function for receiving data from arduino
         if data[0] == '.':
             if data[1] == ',':
                 data = data[2:].split('!')[0].split('@')
-                variables['data1'] = data
+                variables['data1'] = list(map(int, data))
 
 
 def make_global():  # Function for make variables global
@@ -58,19 +58,19 @@ def make_global():  # Function for make variables global
 make_global()   # Make variables global
 
 ser = serial.Serial(search(), 9600)  # Open serial port
-time.sleep(0.3)  # Waiting for port
+time.sleep(1)  # Waiting for port
 
 receiving = Thread(target=receiving_data)   # Create the threading for receive data
 
-time.sleep(2)
+time.sleep(5)
 receiving.start()   # Start the threading for receive data
 print('[INFO] Started')
 
 while True:  # Print variables on display, and switch position to arduino display
-    if len(variables['data1']) == 5:
-        if variables['data1'][4] == '1':
-            ser.write("., [ Automatic  ] @[\1] [\2]  [\1] [\2]!".encode('utf-8'));
-        else:
-            ser.write("., [   Manual   ] @[\2] [\1]  [\2] [\1]!".encode('utf-8'));
-    time.sleep(0.5)
+    #if len(variables['data1']) == 5:
+    #    if variables['data1'][4] == '1':
+    #        ser.write("., [ Automatic  ] @[\1] [\2]  [\1] [\2]!".encode('utf-8'));
+    #    else:
+    #        ser.write("., [   Manual   ] @[\2] [\1]  [\2] [\1]!".encode('utf-8'));
+    #time.sleep(0.5)
     print(variables['data1'])
