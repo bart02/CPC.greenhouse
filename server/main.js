8659bcc7b0b191fa1725e5e2913358355263043a
Artem Batalov <bat.artem2002@ya.ru>
2017-09-18 20:21:25 +0700

    backgroung image in our server

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ var delayTime = 10000;
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-var images = ["http://www.planwallpaper.com/static/images/6768666-1080p-wallpapers.jpg"];
+var images = ["background.jpg"];
 var ready = function(){
     setTimeout(function changeBackground() {
         document.body.style.background = "url('" + images[getRandomInt(0, images.length)] + "') no-repeat center top";
